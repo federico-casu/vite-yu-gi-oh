326fa29f959ac9bd94140d9a16a8c7bce339daf4
federico-casu <federico.casu1101@gmail.com>
2024-02-09 18:48:27 +0100

    feat: added select to filter cards (doesn't work yet); added the number of cards found by filter

--- src/components/main/AppMain.vue
+++ src/components/main/AppMain.vue
@@ -28,7 +28,17 @@ export default {
     <!-- html componente -->
 
     <main class="d-flex">
-        <div class="container d-flex">
+
+        <div class="container">
+            <select name="filter" id="filter">
+                <option value="">Alien</option>
+            </select>
+        </div>
+
+        <div id="card-container" class="container d-flex">
+
+            <h3>Found {{ store.cards.length }} cards</h3>
+
             <Card 
             v-for="(card, index) in store.cards" :key="index" 
             :propsImg="card.card_images[0].image_url_small"
@@ -45,19 +55,40 @@ export default {
 @use '../../assets/styles/partials/variables' as *;
 
 main {
+    flex-direction: column;
+    justify-content: center;
     align-items: center;
     background-color: $bg-orange;
-    padding: 50px 0;
+    padding: 30px 0;
+    gap: 30px;
     // border: 1px solid blue;
 
-    .container {
+    > div:first-child {
+        align-self: start;
+        padding-left: 10px;
+
+        select {
+            padding: 10px 20px;
+            font-size: 1.2rem;
+            border-radius: 10px;
+        }
+    }
+
+    #card-container {
         background-color: white;
         border: 1px solid red;
         height: 90%;
-        padding: 10px;
+        padding: 30px 20px;
 
         flex-wrap: wrap;
         gap: 20px;
+
+        h3 {
+            width: 100%;
+            background-color: $bg-dark;
+            color: white;
+            padding: 20px 10px;
+        }
     }
 
 }
