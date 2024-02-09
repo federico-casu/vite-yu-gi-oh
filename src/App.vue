399e226d5d01d470764f72f438e8bb80a4468d9e
Federico Casu <83126964+federico-casu@users.noreply.github.com>
2024-02-09 14:52:43 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+
+<script>
+
+import NewComp from './components/NewComp.vue';
+
+  export default {
+    components: {
+      NewComp
+    },
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+
+    }
+  }
+
+</script>
+
+<template>
+
+  <NewComp/>
+
+</template>
+
+<style lang="scss">
+
+  @use './assets/styles/general.scss' as *;
+  @use './assets/styles/partials/mixins.scss' as *;
+  @use './assets/styles/partials/variables.scss' as *;
+
+
+
+</style>
---
+++ src/components/NewComp.vue
@@ -0,0 +1,29 @@
+
+<script>
+    // Js componente
+    export default {
+        name: 'NewComp',
+        data() {
+            return {
+                
+            }
+        },
+        methods: {
+            
+        }
+    }
+</script>
+
+
+<template>
+    <!-- html componente -->
+
+    
+</template>
+
+
+<style lang="scss" scoped>
+    /* stile del componente */
+
+    
+</style>
